31f0a0874f3fb34f0adaace3ef68b511e5f876bf
许博然 <xuboran@xuborandeMacBook-Pro.local>
2016-11-15 19:19:37 +0800

    first

---
+++ nothing/nothing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  nothing
+//
+//  Created by 许博然 on 2016/11/15.
+//  Copyright © 2016年 许博然. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
